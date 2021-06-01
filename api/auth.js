661498be2f3192b5953fa246079f4929be48d808
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-01 22:54:13 +0800

    Add api + web

---
+++ api/auth.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const https = require("https");
+const axios = require("axios");
+
+router.post("/login", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://ec2-18-163-183-202.ap-east-1.compute.amazonaws.com/ad/api/auth/login",
+      req.body,
+      {
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json(error.response.data);
+  }
+});
+
+router.get("/userinfo", async (req, res) => {
+  try {
+    const authorization = req.headers.authorization;
+    const response = await axios.get(
+      "https://ec2-18-163-183-202.ap-east-1.compute.amazonaws.com/ad/api/auth/userinfo",
+      {
+        headers: {
+          authorization,
+        },
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json(error.response.data);
+  }
+});
+
+module.exports = router;
