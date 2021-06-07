9f9bb7747e8268ea9fcdd7b394a0d3ab32b93a62
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-07 14:52:19 +0800

    add proxy for local api's

--- routes.js
+++ routes.js
@@ -1,5 +1,21 @@
 module.exports = [
   {
+    route: "/local/ad/api",
+    address: "https://localhost:8082",
+    secure: false,
+    pathRewrite: {
+      "^/local/ad/api": "/",
+    },
+  },
+  {
+    route: "/local/ad/sec/api",
+    address: "https://localhost:8082",
+    secure: false,
+    pathRewrite: {
+      "^/local/ad/sec/api": "/",
+    },
+  },
+  {
     route: "/ad/api",
     address: "https://ec2-18-163-183-202.ap-east-1.compute.amazonaws.com",
     secure: false,
