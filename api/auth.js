de6f9cf0b2c75550c51de93624c39395c9c07c47
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-10 16:39:26 +0800

    changed request method

--- api/auth.js
+++ api/auth.js
@@ -30,7 +30,7 @@ router.post("/logout", (req, res) => {
 
 router.post("/reissue", async (req, res) => {
   try {
-    const response = await axios.get(
+    const response = await axios.post(
       "https://ec2-18-166-169-227.ap-east-1.compute.amazonaws.com/ad/api/auth/reissue",
       {
         httpsAgent: new https.Agent({
